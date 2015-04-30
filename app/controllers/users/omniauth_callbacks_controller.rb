4515b9b6f5b23638aa376046e1c0768db563daa3
Eduardo Figarola <eduardo.figarola@crowdint.com>
2015-04-30 12:45:33 -0500

    Setup devise and omniauth

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,13 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+    @user = User.find_for_google_oauth2(request.env['omniauth.auth'], current_user)
+
+    if @user
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      flash[:alert] = 'You need a crowdint.com account to access this website'
+      redirect_to new_user_session_path
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :trackable, :validatable, :omniauthable
+
+  def self.find_for_google_oauth2(oauth, current_user = nil)
+    if valid_email?(oauth['info']['email'])
+      user = User.where(email: oauth['info']['email']).first
+
+      unless user
+        user                       = User.new
+        user.email                 = oauth['info']['email']
+        user.name                  = oauth['info']['name']
+        user.password              = Devise.friendly_token[0,20]
+        user.password_confirmation = user.password
+        user.save
+      end
+
+      user
+    end
+  end
+
+  def self.valid_email?(email)
+    email.end_with?('crowdint.com')
+  end
+end
