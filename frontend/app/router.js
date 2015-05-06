b845aae306b5c86fbc226d73214d0af02f415a51
Eduardo Figarola <eduardo.figarola@crowdint.com>
2015-05-06 11:45:28 -0500

    Add breakfast detail feedback new route

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
-  this.route('feedback');
+  this.route('feedback', {}, function() {
+   this.route('breakfast', { path: '/breakfast/:id' });
+  });
   this.route('vote');
 });
--- frontend/app/routes/feedback.js
+++ frontend/app/routes/feedback.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('breakfast');
+  },
+
+  actions: {
+    goToFeedbackModal: function(breakfast) {
+      return this.transitionTo('feedback.breakfast', breakfast.id);
+    }
   }
 });
